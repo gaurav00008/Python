227c547692d651813ac6d10a43486f7283ff779a
gaurav00008 <gauravpaneru95@gmail.com>
2026-04-03 18:45:13 +0545

    Variables and Datatypes in Python

--- Lib/main.py
+++ Lib/main.py
@@ -1,5 +1,16 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
 
-print("Hello", name)
-print("You are", age, "years old")
+# print("Hello", name)
+# print("You are", age, "years old")
+
+
+
+# Variables and Data Types
+
+name = "Anish"
+age = 20
+height = 5.9
+is_student = True
+
+print(name, age, height, is_student)
